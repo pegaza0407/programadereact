ce9aea77017eed6d4ac07199948c12f59295e94a
pegaza0407 <pinturaspegaza@hotmail.com>
2024-12-11 11:03:37 -0600

    VENDEDOR

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Nombreetiqueta from './components/ensenarreact.tsx';
 import ProductList from './components/ProductList.tsx';
 import Userform from './components/practicausuarios.tsx';
+import Datosdevendedor from './components/ventas.tsx';
 
 function App() {
   return (
     <div>
-    <Nombreetiqueta></Nombreetiqueta>
-    <Userform></Userform>
+    {/*<Nombreetiqueta></Nombreetiqueta>*/}
+     {/*<Userform></Userform>*/}
+    <Datosdevendedor></Datosdevendedor>
     </div>
   );
 }
---
+++ src/components/ventas.tsx
@@ -0,0 +1,85 @@
+import { getValue } from "@testing-library/user-event/dist/utils";
+import React, { useState } from "react";
+
+interface Vendedor{
+_id:string;
+nom_ferre:string;
+ciudad:string;
+estado:string;
+nombreencargado:string;
+telefono:string;
+label_registro:string;
+fechavisita:Date;
+}
+
+
+const inputEstilo: React.CSSProperties = {
+    margin: '5px',
+    padding: '10px',
+    width: '30%',
+    borderRadius: '5px',
+    border: '1px solid #ccc',
+  };
+
+
+  const buttonEstilo: React.CSSProperties = {
+    padding: '10px 15px',
+    margin: '5px',
+    backgroundColor: '#A70700',
+    color: 'white',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+  };
+
+export default function Datosdevendedor(): React.JSX.Element{
+
+    const[nuevavisita,setNuevavisita]=useState < Omit < Vendedor,'_id'>>({
+        nom_ferre:'',
+        ciudad:'',
+        estado:'',
+        nombreencargado:'',
+        telefono:'',
+        label_registro:'',
+        fechavisita: new Date()});
+
+
+        const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+            const { name, value } = e.target;
+        
+            // Si el input es de tipo 'date', convertimos el valor a un objeto Date.
+            if (name === 'fechavisita') {
+              setNuevavisita((prev) => ({ ...prev, fechavisita: new Date(value) }));
+            } else {
+              setNuevavisita((prev) => ({ ...prev, [name]: value }));
+            }
+          };
+
+
+    return(
+        <div>
+            <h1>REGISTRO VISITAS DE CLIENTES</h1>
+            <form>
+                <input type="text" placeholder="Nombre del negocio" value={nuevavisita.nom_ferre}
+                        onChange={(e)=>setNuevavisita({...nuevavisita,nom_ferre:e.target.value})} style={inputEstilo}/> <br></br>
+                <input type="text" placeholder="Ciudad" value={nuevavisita.ciudad}
+                        onChange={(e)=>setNuevavisita({...nuevavisita,ciudad:e.target.value})}style={inputEstilo}/>
+                <input type="text" placeholder="Estado" value={nuevavisita.estado}
+                        onChange={(e)=>setNuevavisita({...nuevavisita,estado:e.target.value})}style={inputEstilo}/><br></br>
+                <input type="text" placeholder="Nombre del encargado" value={nuevavisita.nombreencargado}
+                        onChange={(e)=>setNuevavisita({...nuevavisita,nombreencargado:e.target.value})}style={inputEstilo}/>
+                <input type="text" placeholder="Telefono" value={nuevavisita.telefono}
+                        onChange={(e)=>setNuevavisita({...nuevavisita,telefono:e.target.value})}style={inputEstilo}/><br></br>
+                <textarea  placeholder="Texto de la visita" value={nuevavisita.label_registro}
+                        onChange={(e)=>setNuevavisita({...nuevavisita,label_registro:e.target.value})}style={inputEstilo}/><br></br>
+                
+                <input type="date" placeholder="Fecha de la visita" 
+                    value={nuevavisita.fechavisita.toISOString().split('T')[0] }
+                    onChange={handleChange} name="fechavisita"/>
+
+                <button type="submit" style={buttonEstilo}>Agregar visita</button>
+            </form>
+        </div>
+
+    );
+}
