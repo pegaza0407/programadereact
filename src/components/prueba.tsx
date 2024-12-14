a3d7afcd9c91a7bf76dd88612b2346a1b90e73fc
pegaza0407 <pinturaspegaza@hotmail.com>
2024-12-13 18:13:51 -0600

    VISITASFALLO

---
+++ src/components/prueba.tsx
@@ -0,0 +1,35 @@
+
+import React, { useState } from 'react';
+
+const FormularioCaptura = () => {
+        // Estado para almacenar el valor del input
+        const [nombre, setNombre] = useState('');
+
+        // Función para manejar el cambio en el input
+        const manejarCambio = (event) => {
+            setNombre(event.target.value);
+        };
+
+  // Función para manejar el envío del formulario
+    const manejarEnvio = (event) => {
+        event.preventDefault(); // Evita que la página se recargue
+        
+        alert( 'Nombre capturado': ${nombre});
+    // Aquí puedes agregar lógica para enviar los datos a un servidor, etc.
+  };
+
+  return (
+    
+    <form>
+        <input  value={nombre}/>Nombre:
+    
+        <button type="submit" >Enviar</button>
+               
+    </form>
+      
+    
+
+  );
+};
+
+export default FormularioCaptura;
